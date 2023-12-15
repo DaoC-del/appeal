cf557d82360e10bc3c0deb3c2b65150a8b353985
pc011 <550164003@qq.com>
2023-12-15 18:26:57 +0900

    提交信息

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:4200' // 或者适用于开发环境的URL
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://daoc-del.github.io/appeal/' // 您的生产环境URL
+};
